579afc48dee7a73feb5f8751d077b00fd0a98050
pradeepbhattarai08 <pradeepbhattarai08@gmail.com>
2024-05-03 05:59:05 +0000

    inittial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+try:
+    S=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print('Socket successfully created');
+except socket.error as err:
+    print(f'Socket creation failded with error {err}')
+
+port=80
+
+try:
+    host_ip=socket.gethostbyname('www.github.com');
+except socket.gaierror:
+        print(f'Error resolving the host {err}')
+        sys.exit()
+
+S.connect((host_ip,port))
+print(f'Socket has successfully connected to github on port = {host_ip}')
+
+
+
